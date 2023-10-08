2b0acb2d342d6a048b5494e2f0ada3ec24628020
Pur1t <purit.te@ku.th>
2023-10-09 00:35:23 +0700

    added file to repo

---
+++ src/main/java/ku/cs/kafe/controller/HomeController.java
@@ -0,0 +1,17 @@
+package ku.cs.kafe.controller;
+
+import org.apache.catalina.security.SecurityConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Anyong");
+        // ต้องคืนค่าเป็นชื่อไฟล์ html template โดยในเมธอดนี้ คืนค่าเป็น home.html
+        return "home";
+    }
+}
